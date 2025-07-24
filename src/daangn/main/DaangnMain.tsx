d2cd62d6ba8ad6808b6129c530f9a578ccf228df
ironjin <cheoljin1234@naver.com>
2025-07-24 21:09:17 +0900

    [feature/daangn] - 화면 스타일 개선

--- src/daangn/main/DaangnMain.tsx
+++ src/daangn/main/DaangnMain.tsx
@@ -10,31 +10,36 @@ const DaangnMain = () => {
   const width = useResponsiveFullWidth();  
 
   return (
-    <Box
-      sx={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100vh',
-        bgcolor: 'background.default',
-        display: 'flex',
-        justifyContent: 'center',
-      }}
-    >
-      <div style={{
-        display: 'block'
-      }}>
-        <DaangnHeader />
-        <main>
+    <div className="main_body">
+      <DaangnHeader />
+      <main className="main_content">
           <div className="wv2v230 pl_20_large pr_20_large pb_4_base pt_4_base">
             <SearchBarMain />
             <CategoryView />
           </div>
         </main>
-      </div>
+    </div>
+    // <Box
+    //   sx={{
+    //     position: 'fixed',
+    //     top: 0,
+    //     left: 0,
+    //     width: '100%',
+    //     height: '100vh',
+    //     bgcolor: 'background.default',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //   }}
+    // >
       
-    </Box>
+    //   <div style={{
+    //     display: 'block'
+    //   }}>
+        
+        
+    //   </div>
+      
+    // </Box>
   );
 };
 
