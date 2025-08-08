a1185b70a00e8e1e8b6363a484c3addbe6736fb9
ironjin <cheoljin1234@naver.com>
2025-08-08 23:06:10 +0900

    [feature/daangn-product] - 왼쪽 상단 브래드컴 추가

--- src/daangn/product/ProductWrap.tsx
+++ src/daangn/product/ProductWrap.tsx
@@ -14,7 +14,26 @@ const ProductWrap = () => {
             <SearchBarSection popullarSearchWords={popullarSearchWords} classes='wv2v230 pl_20_large pr_20_large' />
 
             <main className="prdouct_main_warp">
-                <div>hello</div>
+                <div className="wv2v230 pl_4_base pl_10_small pl_16_medium pl_20_large pr_4_base pr_10_small pr_16_medium pr_20_large backgroundColor_layerDefault width_full_base">
+                    <div className="mt_5_base mt_6_small mt_7_medium">
+                        <nav className="" aria-label="중고거래 검색 페이지 네비게이션">
+                            <ol className="product_main_wrap_bc_ol">
+                                <li className="product_main_wrap_bc_li">
+                                    <a href="/kr/?in=%EB%93%B1%EC%B4%8C%EB%8F%99-6051" className="product_main_wrap_bc_a" data-discover="true">홈</a>
+                                    <span className="product_main_wrap_bc_a_span" role="presentation">
+                                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" data-seed-icon="true" data-seed-icon-version="0.0.10" width="12" height="12"><g><path fill-rule="evenodd" clip-rule="evenodd" d="M7.93106 2.60286C7.47398 3.08276 7.49248 3.84233 7.97238 4.29941L16.06 12.0024L7.97245 19.7043C7.49251 20.1614 7.47396 20.921 7.93101 21.4009C8.38806 21.8808 9.14763 21.8994 9.62756 21.4423L18.6276 12.8715C18.8654 12.645 19 12.3309 19 12.0025C19 11.6741 18.8654 11.36 18.6276 11.1335L9.62762 2.56153C9.14771 2.10445 8.38814 2.12296 7.93106 2.60286Z" fill="currentColor"></path></g></svg>
+                                    </span>
+                                </li>
+                                <li className="product_main_wrap_bc_li">
+                                    <span className="product_main_wrap_bc_sub_span_large" aria-current="page">중고거래</span>
+                                    <span className="product_main_wrap_bc_sub_span_small" role="presentation">
+                                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" data-seed-icon="true" data-seed-icon-version="0.0.10" width="12" height="12"><g><path fill-rule="evenodd" clip-rule="evenodd" d="M7.93106 2.60286C7.47398 3.08276 7.49248 3.84233 7.97238 4.29941L16.06 12.0024L7.97245 19.7043C7.49251 20.1614 7.47396 20.921 7.93101 21.4009C8.38806 21.8808 9.14763 21.8994 9.62756 21.4423L18.6276 12.8715C18.8654 12.645 19 12.3309 19 12.0025C19 11.6741 18.8654 11.36 18.6276 11.1335L9.62762 2.56153C9.14771 2.10445 8.38814 2.12296 7.93106 2.60286Z" fill="currentColor"></path></g></svg>
+                                    </span>
+                                </li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
             </main>
         </div>
     );
